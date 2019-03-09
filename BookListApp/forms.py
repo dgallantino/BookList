1560370b1a2262d33f699f4bedac2e86fcec8dd5
dgallantino <dimas.gallantino@gmail.com>
2019-03-09 22:02:20 +0700

    add date selector

--- BookListApp/forms.py
+++ BookListApp/forms.py
@@ -10,19 +10,22 @@ class BookCreationForm(BookBaseForm):
     """Creation Form"""
     def __init__(self, *args, **kwargs):
         super(BookCreationForm, self).__init__(*args, **kwargs)
+        #styling by atributes
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
             visible.field.widget.attrs['placeholder'] = visible.field.label
-            visible.field.widget.attrs['required'] = True
+        self.fields.get("date_published").widget.attrs["placeholder"]="Date published (dd/mm/yyyy)"
     class Media:
         css = {
             'all':(
                 'BookListApp/css/add-form.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css',
             )
         }
 
         js = (
             'BookListApp/js/add-form.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js',
         )
 
 
